dc8aa5fbeea3746d413f37f9ba5ba945049b74dd
mbormash <mbormash@gmail.com>
2019-08-30 23:44:49 +0300

    Tests for subtract operation created.

--- src/test/java/tests/controller/ControllerTest.java
+++ src/test/java/tests/controller/ControllerTest.java
@@ -25,6 +25,7 @@ public class ControllerTest extends RobotControl {
         backspaceTests();
         clearTests();
         addTests();
+        subtractTests();
     }
 
     /**
@@ -578,8 +579,7 @@ public class ControllerTest extends RobotControl {
     /**
      * Tests for add operation.
      */
-    @Test
-    public void addTests() {
+    private void addTests() {
         //standard cases
         checkTyped("+", "0", "0 +");
         checkTyped("0+", "0", "0 +");
@@ -697,6 +697,130 @@ public class ControllerTest extends RobotControl {
         clickButtons("+");
         assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
         assertEquals("0 +", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+    }
+
+    /**
+     * Tests for subtract operation.
+     */
+    @Test
+    public void subtractTests() {
+        //standard cases
+        checkTyped("-", "0", "0 -");
+        checkTyped("0-", "0", "0 -");
+        checkTyped("1-", "1", "1 -");
+        checkTyped("2-", "2", "2 -");
+        checkTyped("3-", "3", "3 -");
+        checkTyped("4-", "4", "4 -");
+        checkTyped("5-", "5", "5 -");
+        checkTyped("6-", "6", "6 -");
+        checkTyped("7-", "7", "7 -");
+        checkTyped("8-", "8", "8 -");
+        checkTyped("9-", "9", "9 -");
+
+        //without comma
+        checkTyped("17-", "17", "17 -");
+        checkTyped("256-", "256", "256 -");
+
+        //with comma
+        checkTyped("11515-", "11,515", "11515 -");
+        checkTyped("734347956-", "734,347,956", "734347956 -");
+
+        //several add operations
+        checkTyped("1-2-3", "3",
+                "1 - 2 -");
+        checkTyped("1-2-3-", "-4",
+                "1 - 2 - 3 -");
+        checkTyped("100-1000-10000-100000", "100,000",
+                "100 - 1000 - 10000 -");
+        checkTyped("100-1000-10000-100000-", "-110,900",
+                "100 - 1000 - 10000 - 100000 -");
+
+        //after dot
+        checkTyped("62.-", "62", "62 -");
+        checkTyped("623626.-", "623,626", "623626 -");
+
+        //after negate
+        checkTyped("866~-", "-866", "-866 -");
+        checkTyped("98791480~-", "-98,791,480", "-98791480 -");
+
+        //after another unary
+        checkTyped("8^-", "64", "sqr(8) -");
+        checkTyped("123^-", "15,129", "sqr(123) -");
+        checkTyped("49@-", "7", "√(49) -");
+        checkTyped("3600000000@-", "60,000", "√(3600000000) -");
+        checkTyped("1;-", "1", "1/(1) -");
+        checkTyped("0.000001;-", "1,000,000", "1/(0.000001) -");
+
+        //in a row
+        checkTyped("55--", "55", "55 -");
+        checkTyped("1567-----", "1,567", "1567 -");
+
+        //after another binary
+        checkTyped("16+-", "16", "16 -");
+        checkTyped("7624+-", "7,624", "7624 -");
+        checkTyped("564*-", "564", "564 -");
+        checkTyped("6522456*-", "6,522,456", "6522456 -");
+        checkTyped("12/-", "12", "12 -");
+        checkTyped("344363/-", "344,363", "344363 -");
+
+        //after percent
+        checkTyped("78%-", "0", "0 -");
+        checkTyped("56245%-", "0", "0 -");
+
+        //after equals
+        checkTyped("73=-", "73", "73 -");
+        checkTyped("532626=-", "532,626", "532626 -");
+
+        //calculating
+        checkTyped("92-54-", "38", "92 - 54 -");
+        checkTyped("8898-123-", "8,775", "8898 - 123 -");
+
+        checkTyped("913-14.1-", "898.9", "913 - 14.1 -");
+        checkTyped("7362.5-638.1-", "6,724.4", "7362.5 - 638.1 -");
+
+        //engineers
+        checkTyped("-9000000000000000-1000000000000000-", "-1.e+16",
+                "0 - 9000000000000000 - 1000000000000000 -");
+        checkTyped("-9000000000000000-1000000000000000-1-", "-1.e+16",
+                "0 - 9000000000000000 - 1000000000000000 - 1 -");
+        checkTyped("-9000000000000000-1000000000000000-4-", "-1.e+16",
+                "0 - 9000000000000000 - 1000000000000000 - 4 -");
+        checkTyped("-9000000000000000-1000000000000000-4-1-", "-1.000000000000001e+16",
+                "0 - 9000000000000000 - 1000000000000000 - 4 - 1 -");
+        checkTyped("-9000000000000000-1000000000000000-5-", "-1.000000000000001e+16",
+                "0 - 9000000000000000 - 1000000000000000 - 5 -");
+        checkTyped("-9999999999999999-9999999999999999-", "-2.e+16",
+                "0 - 9999999999999999 - 9999999999999999 -");
+
+        //after clear text
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("124");
+        clickOn(getButtonBySelector(CLEAR_TEXT_ID));
+        clickButtons("-");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 -", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("564-");
+        clickOn(getButtonBySelector(CLEAR_TEXT_ID));
+        clickButtons("-");
+        assertEquals("564", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("564 - 0 -", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        //after clear all
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("124");
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("-");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 -", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("564-");
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("-");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 -", getLabeledBySelector(EQUATION_LABEL_ID).getText());
     }
 
     /**
