acee9ff7d2d327ef5383fc9c09ef3311157a7c39
mbormash <mbormash@gmail.com>
2019-08-31 02:57:38 +0300

    Tests for multiply operation created.

--- src/test/java/tests/controller/ControllerTest.java
+++ src/test/java/tests/controller/ControllerTest.java
@@ -26,6 +26,7 @@ public class ControllerTest extends RobotControl {
         clearTests();
         addTests();
         subtractTests();
+        multiplyTests();
     }
 
     /**
@@ -702,8 +703,7 @@ public class ControllerTest extends RobotControl {
     /**
      * Tests for subtract operation.
      */
-    @Test
-    public void subtractTests() {
+    private void subtractTests() {
         //standard cases
         checkTyped("-", "0", "0 -");
         checkTyped("0-", "0", "0 -");
@@ -725,7 +725,7 @@ public class ControllerTest extends RobotControl {
         checkTyped("11515-", "11,515", "11515 -");
         checkTyped("734347956-", "734,347,956", "734347956 -");
 
-        //several add operations
+        //several subtract operations
         checkTyped("1-2-3", "3",
                 "1 - 2 -");
         checkTyped("1-2-3-", "-4",
@@ -822,6 +822,134 @@ public class ControllerTest extends RobotControl {
         assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
         assertEquals("0 -", getLabeledBySelector(EQUATION_LABEL_ID).getText());
     }
+
+    /**
+     * Tests for multiply operation.
+     */
+    private void multiplyTests() {
+        //standard cases
+        checkTyped("*", "0", "0 ×");
+        checkTyped("0*", "0", "0 ×");
+        checkTyped("1*", "1", "1 ×");
+        checkTyped("2*", "2", "2 ×");
+        checkTyped("3*", "3", "3 ×");
+        checkTyped("4*", "4", "4 ×");
+        checkTyped("5*", "5", "5 ×");
+        checkTyped("6*", "6", "6 ×");
+        checkTyped("7*", "7", "7 ×");
+        checkTyped("8*", "8", "8 ×");
+        checkTyped("9*", "9", "9 ×");
+
+        //without comma
+        checkTyped("17*", "17", "17 ×");
+        checkTyped("256*", "256", "256 ×");
+
+        //with comma
+        checkTyped("11515*", "11,515", "11515 ×");
+        checkTyped("734347956*", "734,347,956", "734347956 ×");
+
+        //several multiply operations
+        checkTyped("1*2*3", "3",
+                "1 × 2 ×");
+        checkTyped("1*2*3*", "6",
+                "1 × 2 × 3 ×");
+        checkTyped("100*1000*10000*100000", "100,000",
+                "100 × 1000 × 10000 ×");
+        checkTyped("100*1000*10000*100000*", "100,000,000,000,000",
+                "100 × 1000 × 10000 × 100000 ×");
+
+        //after dot
+        checkTyped("62.*", "62", "62 ×");
+        checkTyped("623626.*", "623,626", "623626 ×");
+
+        //after negate
+        checkTyped("866~*", "-866", "-866 ×");
+        checkTyped("98791480~*", "-98,791,480", "-98791480 ×");
+
+        //after another unary
+        checkTyped("8^*", "64", "sqr(8) ×");
+        checkTyped("123^*", "15,129", "sqr(123) ×");
+        checkTyped("49@*", "7", "√(49) ×");
+        checkTyped("3600000000@*", "60,000", "√(3600000000) ×");
+        checkTyped("1;*", "1", "1/(1) ×");
+        checkTyped("0.000001;*", "1,000,000", "1/(0.000001) ×");
+
+        //in a row
+        checkTyped("55**", "55", "55 ×");
+        checkTyped("1567*****", "1,567", "1567 ×");
+
+        //after another binary
+        checkTyped("16+*", "16", "16 ×");
+        checkTyped("7624+*", "7,624", "7624 ×");
+        checkTyped("564-*", "564", "564 ×");
+        checkTyped("6522456-*", "6,522,456", "6522456 ×");
+        checkTyped("12/*", "12", "12 ×");
+        checkTyped("344363/*", "344,363", "344363 ×");
+
+        //after percent
+        checkTyped("78%*", "0", "0 ×");
+        checkTyped("56245%*", "0", "0 ×");
+
+        //after equals
+        checkTyped("73=*", "73", "73 ×");
+        checkTyped("532626=*", "532,626", "532626 ×");
+
+        //calculating
+        checkTyped("92*5*", "460", "92 × 5 ×");
+        checkTyped("8898*123*", "1,094,454", "8898 × 123 ×");
+
+        checkTyped("913*1.01*", "922.13", "913 × 1.01 ×");
+        checkTyped("7362.5*638.1*", "4,698,011.25", "7362.5 × 638.1 ×");
+
+        //engineers
+        checkTyped("9000000000000000*1000000000000000*", "9.e+30",
+                "9000000000000000 × 1000000000000000 ×");
+        checkTyped("9999999999999999*1000000000000000*", "9.999999999999999e+30",
+                "9999999999999999 × 1000000000000000 ×");
+        checkTyped("9000000000000000*1000000000000000*4*", "3.6e+31",
+                "9000000000000000 × 1000000000000000 × 4 ×");
+        checkTyped("0.9999999999999999*0.9999999999999999*", "0.9999999999999998",
+                "0.9999999999999999 × 0.9999999999999999 ×");
+        checkTyped("0.9999999999999999*0.1*", "0.1",
+                "0.9999999999999999 × 0.1 ×");
+        checkTyped("0.9999999999999999*0.5*", "0.5",
+                "0.9999999999999999 × 0.5 ×");
+        checkTyped("0.9999999999999999*0.6*", "0.5999999999999999",
+                "0.9999999999999999 × 0.6 ×");
+        checkTyped("9999999999999999*9999999999999999*", "9.999999999999998e+31",
+                "9999999999999999 × 9999999999999999 ×");
+
+        //after clear text
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("124");
+        clickOn(getButtonBySelector(CLEAR_TEXT_ID));
+        clickButtons("*");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 ×", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("564*");
+        clickOn(getButtonBySelector(CLEAR_TEXT_ID));
+        clickButtons("*");
+        assertEquals("564", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("564 × 0 ×", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        //after clear all
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("124");
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("*");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 ×", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("564*");
+        clickOn(getButtonBySelector(CLEAR_ALL_ID));
+        clickButtons("*");
+        assertEquals("0", getLabeledBySelector(SCREEN_LABEL_ID).getText());
+        assertEquals("0 ×", getLabeledBySelector(EQUATION_LABEL_ID).getText());
+    }
+    
 
     /**
      * Checks that screen label has required text after clicking on buttons.
