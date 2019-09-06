fd20cea9fbbf70134be41d25aa86bd40f887a6be
Mykhailo <mbormash@gmail.com>
2019-09-06 12:46:07 +0300

    Launcher refactored.

--- src/main/java/com/implemica/bormashenko/calculator/Launcher.java
+++ src/main/java/com/implemica/bormashenko/calculator/Launcher.java
@@ -2,18 +2,12 @@ package com.implemica.bormashenko.calculator;
 
 import com.implemica.bormashenko.calculator.view.View;
 import javafx.application.Application;
-import javafx.scene.Parent;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
 
 /**
  * Main class for application.
  *
  * @author Mykhailo Bormashenko
- * @todo refactoring
  */
 public class Launcher extends Application {
 
